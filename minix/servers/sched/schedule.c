d9baf34b45795cf99af02c989d985d4e8fc6c7bb
Natã Almeida <103054531+nsmalmeida@users.noreply.github.com>
2025-07-03 19:18:42 -0300

    Update schedule.c
    mudanca 1 - aplicacao padrao

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -96,6 +96,11 @@ int do_noquantum(message *m_ptr)
 	}
 
 	rmp = &schedproc[proc_nr_n];
+	
+	if (rmp->priority == USER_Q) {
+		return OK;
+	}
+	
 	if (rmp->priority < MIN_USER_Q) {
 		rmp->priority += 1; /* lower priority */
 	}
@@ -274,6 +279,10 @@ int do_nice(message *m_ptr)
 		return EINVAL;
 	}
 
+	if (rmp->priority == USER_Q) {
+		return OK;
+	}
+
 	/* Store old values, in case we need to roll back the changes */
 	old_q     = rmp->priority;
 	old_max_q = rmp->max_priority;
@@ -357,6 +366,9 @@ void balance_queues(void)
 
 	for (proc_nr=0, rmp=schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
 		if (rmp->flags & IN_USE) {
+			if (rmp->priority == USER_Q) {
+				continue;
+			}
 			if (rmp->priority > rmp->max_priority) {
 				rmp->priority -= 1; /* increase priority */
 				schedule_process_local(rmp);
